c2647a94260f242a16c8f4f551947485819d6d54
Y3p4gu <31299202+yepagu@users.noreply.github.com>
2022-11-29 10:27:37 -0500

    Footer: agregado el boton flotante de Whatsapp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,7 +130,10 @@
         </div>
       </div>
     </div>
+    @include('layouts.wtp') 
   </footer><!-- End  Footer -->
+
+  
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
